8d58899ca7a5789630d5d03cdfd1e3291675fccf
Matt Steele <orphum@gmail.com>
2014-11-01 12:47:32 -0500

    Make the Sphero speak when it's in the air

--- apps/airborne.js
+++ apps/airborne.js
@@ -1,11 +1,24 @@
 var s = require('../lib/sphero');
+var exec = require('child_process').exec;
+
+var airborne = false;
 s.connect().then(function(ball) {
   ball.setDataStreaming(['accelOne'], { n: 13, m: 1, pcnt: 0});
   ball.on('data', function(data) {
     var a = data[0];
     if (a < 50) {
+      if (!airborne) {
+        airborne = true;
+        if (process.platform === 'darwin') {
+          var cmd = 'say -v Victoria wheeeeeeee!';
+          exec(cmd);
+        }
+      }
       ball.setRGB(0xFF0000);
     } else {
+      if (airborne) {
+        airborne = false;
+      }
       ball.setRGB(0x00FF00);
     }
   });
